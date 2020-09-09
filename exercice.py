b60bb23f9d63489bc48a2f6f8dea7664942f25be
tlusignan <thomas.lusignan@gmail.com>
2020-09-09 14:44:25 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -4,8 +4,8 @@ def majuscule(mot):
     resultat = ''
     for lettre in mot:
         # TODO completer la fonction ici
-        
-        
+        ordlettre = ord()
+
         resultat += lettre
     return mot
 
